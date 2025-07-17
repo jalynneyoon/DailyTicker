fb84e9d9e419daaa76ea516d6734ac98a182f28c
jalynneyoon <jalynneyoon@gmail.com>
2025-07-17 13:07:10 +0900

    Initial Commit

---
+++ DailyTicker/DailyTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTickerApp.swift
+//  DailyTicker
+//
+//  Created by JohyeonYoon on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
